430c4fb71d06951df5e8b7903969e70679243f34
Tony Brewer <tonybrewer@wildfirecybernetics.com>
2010-10-14 16:05:02 -0700

    WildFire Cybernetics initial commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,4 @@
+class ContactsController < ApplicationController
+  # GET /contacts
+  # GET /contacts.xml
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WildfireCybernetics::Application.config.secret_token = '44a3a0f2fd168cca9a66f3c1dfe6e1846579d76d3e349e42b6d19a189d98dce2bd592291728fc8cef6752841ae8af2b7bb66804c6bcaca4f9fedefa7978896cc'
